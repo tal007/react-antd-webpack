71dd754b494dff7ea6b0d6752d697dfdd063de99
tal007 <mrliu819@foxmail.com>
2020-04-20 10:07:35 +0800

    Input 事件清除输入内容

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,7 +3,7 @@ import Header from '@comp/Header';
 class Layout extends React.Component {
   render() {
     return (
-      <section className="layout">
+      <section className="layout" style={{ margin: 20 }}>
         <Header />
         {this.props.children}
       </section>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,25 @@
 import Footer from '@comp/Footer';
+import { useRef } from 'react';
+import { Input, Card, Button, PageHeader } from 'antd';
 
-const Home = () => (
-  <div>
-    Home
-    <Footer />
-  </div>
-);
+const Home = () => {
+  const inputNode = useRef('');
+
+  function handleClick() {
+    console.log(inputNode.current);
+    inputNode.current.setState({ value: '' });
+  }
+
+  return (
+    <div>
+      <PageHeader title="Home" />
+      <Card title="Input 通过事件清空内容">
+        <Input ref={inputNode} placeholder="测试" />
+        <Button onClick={handleClick}>点击按钮</Button>
+      </Card>
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
